ffff53692741473cb2ce5c4ee9267018915a0814
Lucas Veber de Brida <lucasveberdebrida@gmail.com>
2019-09-08 17:29:46 -0300

    Adicionando timestamp no product schema

--- server/src/modules/product/productModel.js
+++ server/src/modules/product/productModel.js
@@ -3,15 +3,18 @@ const Enum = require('../../util/enum/Enum');
 
 exports.productStatus = new Enum(['ACTIVE', 'INACTIVE', 'DELETED']);
 
-const productSchema = new mongoose.Schema({
-  name: { type: String, unique: true, required: true },
-  description: { type: String, required: true },
-  quantity: { type: Number, required: true },
-  status: {
-    type: String,
-    enum: this.productStatus.list(),
-    default: this.productStatus.ACTIVE,
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, unique: true, required: true },
+    description: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    status: {
+      type: String,
+      enum: this.productStatus.list(),
+      default: this.productStatus.ACTIVE,
+    },
   },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Product', productSchema);
