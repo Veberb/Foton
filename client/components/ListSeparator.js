5bce0efe5e225ca650c8234681c40f947d442e14
Lucas Veber de Brida <lucasveberdebrida@gmail.com>
2019-10-07 23:44:54 -0300

    Component para separar produtos

---
+++ client/components/ListSeparator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function ListSeparator() {
+  return (
+    <View
+      style={{
+        height: 1,
+        width: '100%',
+        backgroundColor: '#CED0CE',
+      }}
+    />
+  );
+}
